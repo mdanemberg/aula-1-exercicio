a843acb7c48acdcf94b581e61c286774d7d16e71
Matheus Danemberg <matheus@nave.rs>
2019-05-11 11:53:59 -0300

    delete tweet

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -17,9 +17,18 @@ export default class Tweet {
           <div class="content">
             ${this.text}
           </div>
+          <button class="button is-dark is-pulled-right">Excluir</button>
         </div>
       </div>
     `
+  }
+
+  deleteTweet () {
+    this.tweet.remove()
+  }
+
+  binds () {
+    this.deleteButton.addEventListener('click', this.deleteTweet.bind(this))
   }
 
   render () {
@@ -28,6 +37,9 @@ export default class Tweet {
     const cardContainer = this.card(this.name, this.text)
     tweet.innerHTML = cardContainer
     this.tweet = tweet
+    this.deleteButton = this.tweet.getElementsByTagName('button')[0]
+    this.binds()
+
     return tweet
   }
 }
