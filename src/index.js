403ae3e38416a88699738528bdc29966e04f1a63
Matheus Danemberg <matheus@nave.rs>
2019-05-11 09:15:19 -0300

    fix content

--- src/index.js
+++ src/index.js
@@ -1,30 +1,21 @@
 import TweetList from './components/TweetList'
-import TweetForm from './components/TweetForm'
 import Tweet from './components/Tweet'
 
 const tweetsContainer = 'tweets-container'
 const list = new TweetList({tweetsContainer})
 
-const form = 'form-container'
-const tweetForm = new TweetForm({
-  form,
-  tweetsContainer
-})
-tweetForm.render()
-tweetForm.binds()
+const form = document.getElementById('tweet-form')
+form.onsubmit = (e) => {
+  e.preventDefault()
+  const name = document.getElementsByName('name')[0].value
+  const text = document.getElementsByName('tweet')[0].value
 
-// const form = document.getElementById('tweet-form')
-// form.onsubmit = (e) => {
-//   e.preventDefault()
-//   const name = document.getElementsByName('name')[0].value
-//   const text = document.getElementsByName('tweet')[0].value
+  const newTweet = new Tweet({
+    name,
+    text
+  })
 
-//   const newTweet = new Tweet({
-//     name,
-//     text
-//   })
-
-//   list.container.insertBefore(newTweet.render(), list.container.firstChild)
-// }
+  list.container.insertBefore(newTweet.render(), list.container.firstChild)
+}
 
 list.render()
