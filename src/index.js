1448f715ea2eadf6f5315c5c30293b4d3beb2a39
Matheus Danemberg <matheus@nave.rs>
2019-05-11 09:13:51 -0300

    fix content

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import TweetList from './components/TweetList'
 import TweetForm from './components/TweetForm'
-import Tweet from './components/Tweet'
 
 const tweetsContainer = 'tweets-container'
 const list = new TweetList({tweetsContainer})
@@ -12,19 +11,5 @@ const tweetForm = new TweetForm({
 })
 tweetForm.render()
 tweetForm.binds()
-
-// const form = document.getElementById('tweet-form')
-// form.onsubmit = (e) => {
-//   e.preventDefault()
-//   const name = document.getElementsByName('name')[0].value
-//   const text = document.getElementsByName('tweet')[0].value
-
-//   const newTweet = new Tweet({
-//     name,
-//     text
-//   })
-
-//   list.container.insertBefore(newTweet.render(), list.container.firstChild)
-// }
 
 list.render()
