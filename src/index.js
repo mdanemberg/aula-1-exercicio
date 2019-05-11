f8b77e9891a1aa1af997471a37c9df6f4c0ba34d
Matheus Danemberg <matheus@nave.rs>
2019-05-11 10:31:29 -0300

    validate form

--- src/index.js
+++ src/index.js
@@ -7,15 +7,20 @@ const list = new TweetList({tweetsContainer})
 const form = document.getElementById('tweet-form')
 form.onsubmit = (e) => {
   e.preventDefault()
-  const name = document.getElementsByName('name')[0].value
-  const text = document.getElementsByName('tweet')[0].value
+  const name = document.getElementsByName('name')[0]
+  const text = document.getElementsByName('tweet')[0]
 
-  const newTweet = new Tweet({
-    name,
-    text
-  })
+  if (name.value && text.value) {
+    const newTweet = new Tweet({
+      name: name.value,
+      text: text.value
+    })
 
-  list.container.insertBefore(newTweet.render(), list.container.firstChild)
+    name.value = ''
+    text.value = ''
+
+    list.container.insertBefore(newTweet.render(), list.container.firstChild)
+  }
 }
 
 list.render()
